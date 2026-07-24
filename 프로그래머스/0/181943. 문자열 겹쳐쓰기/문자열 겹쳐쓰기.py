ae82f3dcccb3bad0b623b31c3e6511e149cf1d96
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-07-24 11:06:31 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,4 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    answer = ''
-    str1 = my_string[:s]
-    return str1 + overwrite_string + my_string[s + len(overwrite_string):]
+    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
