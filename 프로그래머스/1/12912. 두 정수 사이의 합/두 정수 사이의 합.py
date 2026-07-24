7a685b8ab5b554a55feea65ac4cf03f7d936334c
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-07-24 12:41:13 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 503.54 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,8 +1,7 @@
 def solution(a, b):
     answer = 0
-    start = min(a,b)
-    end = max(a,b)
-    
-    for i in range(start, end + 1):
-        answer +=  i
+    start = min(a, b)
+    end = max(a, b)
+    for i in range(start,end+1):
+        answer += i
     return answer
